3c56d1716cf507b81e3ff5d224f9526eb89bf936
vthwang <vincent@fishboneapps.com>
2026-06-18 14:18:56 -0700

    Clear Origin header on mediator WebSocket proxy
    NGINX forwards the client's Origin header by default, which caused the
    mediator backend to reject WebSocket upgrades with 400 Bad Request.
    Setting Origin to empty suppresses the check.
    
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- scripts/setup-explore.sh
+++ scripts/setup-explore.sh
@@ -152,6 +152,7 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$host;
+        proxy_set_header Origin "";
         proxy_read_timeout 3600s;
     }
 
